d4e098cfbb3961a67e12f275eafd2a8d9084bac2
Earl Tankard, Jr <theflashtank21@gmail.com>
2022-10-04 17:06:48 -0400

    feat: adding pre-push hook

--- testing/test_hello.py
+++ testing/test_hello.py
@@ -1,7 +1,9 @@
+"""Pytests for hello()"""
 from hello import hello
 
 
-def test_main():
+def test_hello():
+    """Tests for `hello()`"""
     assert hello() == "Hello John!"
     assert hello("Jill") == "Hello Jill!"
     assert type(hello()) == str
