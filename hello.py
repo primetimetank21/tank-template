55da4375fca6b4441fa4251ed3f3219bd81d5d22
TanK <theflashtank21@gmail.com>
2024-02-08 16:58:16 -0500

    test: improved code testing coverage

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
-def hello(name:str="John") -> str:
+def hello(name: str = "John") -> str:
     """Say hello to `name`"""
-    phrase:str = f"Hello {name}!"
+    phrase: str = f"Hello {name}!"
     print(phrase)
     return phrase
 
 
-if __name__ == "__main__":
-    hello()
+def main() -> None:
+    if __name__ == "__main__":
+        hello()
+
+
+main()
--- testing/test_hello.py
+++ testing/test_hello.py
@@ -1,10 +1,21 @@
 """Pytests for hello()"""
-from hello import hello
+from typing import cast
+import hello
+import mock  # type:ignore
+from hello import hello as hello_func, main
 
 
-def test_hello():
+def test_hello() -> None:
     """Tests for `hello()`"""
-    assert hello() == "Hello John!"
-    assert hello("Jill") == "Hello Jill!"
-    assert hello(name="Jill") == "Hello Jill!"
-    assert isinstance(hello(), str)
+    assert hello_func() == "Hello John!"
+    assert hello_func("Jill") == "Hello Jill!"
+    assert hello_func(name="Jill") == "Hello Jill!"
+    assert isinstance(hello_func(), str)
+
+
+def test_main() -> None:
+    with mock.patch.object(hello, attribute="main", return_value=None):
+        with mock.patch.object(hello, "__name__", "__main__"):
+            return_val = cast(None, main())
+
+            assert return_val is None
